f1a5718e47746a931707a29d47e01f7ebec91add
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-03-17 08:57:26 +0100

    sweep attack fix

--- src/main/java/fuzs/universalenchants/mixin/PlayerMixin.java
+++ src/main/java/fuzs/universalenchants/mixin/PlayerMixin.java
@@ -1,7 +1,6 @@
 package fuzs.universalenchants.mixin;
 
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -12,7 +11,6 @@ import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity {
@@ -20,9 +18,12 @@ public abstract class PlayerMixin extends LivingEntity {
         super(p_20966_, p_20967_);
     }
 
-    @ModifyVariable(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/world/entity/LivingEntity;)I"), ordinal = 3)
+    @ModifyVariable(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"), ordinal = 3)
     public boolean attack$storeCanPerformSweepAction(boolean canPerformSweepAction) {
+        // this will only be called when sweeping is allowed to happen and only the check for SwordItem is missing, since that's the behavior we want to change
+        // next line should always be false since the variable is not modified before this, but just keep it to be safe with other mods
         if (canPerformSweepAction) return true;
+        // we only handle sweeping edge case, all swords independent of enchantment are still handled by vanilla after this
         ItemStack itemstack = this.getItemInHand(InteractionHand.MAIN_HAND);
         return EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SWEEPING_EDGE, itemstack) > 0;
     }
